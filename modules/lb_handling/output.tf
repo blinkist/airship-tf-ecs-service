d081443f995609306a846ed8c729a9f45beb16d6
Jamie Nelson <jamie@theinstillery.com>
2018-11-21 13:13:42 +0000

    Cleaning up module

--- modules/lb_handling/output.tf
+++ modules/lb_handling/output.tf
@@ -2,6 +2,10 @@ output "lb_target_group_arn" {
   value = "${join("",coalescelist(list(local.tg_arn),aws_lb_target_group.service_alb.*.arn, aws_lb_target_group.service_nlb.*.arn))}"
 }
 
+output "lb_target_group_arn_suffix" {
+  value = "${join("",coalescelist(aws_lb_target_group.service_alb.*.arn_suffix, aws_lb_target_group.service_nlb.*.arn_suffix))}"
+}
+
 # This is an output the ecs_service depends on. This to make sure the target_group is attached to an alb before adding to a service. The actual content is useless
 output "aws_lb_listener_rules" {
   value = ["${concat(aws_lb_listener_rule.host_based_routing.*.arn,aws_lb_listener_rule.host_based_routing_custom_listen_host.*.arn, list())}"]
