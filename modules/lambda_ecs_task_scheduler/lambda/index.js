0d976589fcfdc3620c4bfa9a8dba040f2fed3b2a
maartenvanderhoef <maarten@blinkist.com>
2018-12-07 10:04:37 +0100

    new try with async

--- modules/lambda_ecs_task_scheduler/lambda/index.js
+++ modules/lambda_ecs_task_scheduler/lambda/index.js
@@ -1,80 +1,57 @@
 const AWS = require("aws-sdk");
 const ecs = new AWS.ECS();
 
-exports.handler = function(event, context, callback) {
-  function AirshipLambdaError(message) {
-    this.name = "AirshipLambdaError";
-    this.message = message;
-  }
-  AirshipLambdaError.prototype = new Error();
+exports.handler = async (event, context, callback) => {
+    const ecs_cluster = event.ecs_cluster;
+    const ecs_service = event.ecs_service;
 
-  const ecs_cluster = event.ecs_cluster;
-  const ecs_service = event.ecs_service;
+    const [dserr, dsdata] = await ecs.describeServices({ cluster: ecs_cluster, services: [ecs_service] });
 
-  ecs.describeServices(
-      {cluster : ecs_cluster, services : [ ecs_service ]},
-      function(dserr, dsdata) {
-        if (dserr) {
-          console.log("Unable to retrieve service definition for:",
-                      ecs_service);
-          context.fail(dserr, dserr.stack);
+    if (dserr) {
+        console.log("Unable to retrieve service definition for:", ecs_service);
+        context.fail(dserr, dserr.stack);
+    }
+    if (dsdata.services.length > 1) {
+        throw new Error("multiple services with name %s found in cluster %s" % ecs_service, ecs_cluster);
+    }
+    if (dsdata.services.length < 1) {
+        throw new Error("Could not find service");
+    }
+
+    const taskDefinition = dsdata.services[0].taskDefinition;
+
+    const [dtderr, dtddata] = await ecs.describeTaskDefinition({ taskDefinition: taskDefinition }).promise();
+    if (dtderr) {
+        throw dtderr;
+    }
+    if (dtddata.taskDefinition.containerDefinitions.length !== 1) {
+        throw new Error("only a single container is supported per task definition");
+    }
+
+    const started_by = event.started_by;
+
+    const networkConfiguration = dsdata.services[0].networkConfiguration;
+    const launchType = dsdata.services[0].launchType;
+
+    const params = {
+        taskDefinition: taskDefinition,
+        networkConfiguration: networkConfiguration,
+        cluster: ecs_cluster,
+        count: 1,
+        launchType: launchType,
+        startedBy: started_by,
+        overrides: event.overrides
+    };
+
+    const [err, data] = await ecs.runTask(params).promise();
+    if (err) {
+        if (err.code == "ConditionalCheckFailedException") {
+            callback("duplicated execution: " + JSON.stringify(event));
         } else {
-          if (dsdata.services.length > 1) {
-            throw new AirshipLambdaError(
-                "multiple services with name %s found in cluster %s" %
-                    ecs_service,
-                ecs_cluster);
-          } else if (dsdata.services.length < 1) {
-            throw new AirshipLambdaError("Could not find service");
-          }
-
-          const taskDefinition = dsdata.services[0].taskDefinition;
-
-          ecs.describeTaskDefinition(
-              {taskDefinition : taskDefinition}, function(dtderr, dtddata) {
-                if (dtderr) {
-                  throw dtderr;
-                } else {
-                  if (dtddata.taskDefinition.containerDefinitions.length !==
-                      1) {
-                    throw new AirshipLambdaError(
-                        "only a single container is supported per task definition");
-                  }
-
-                  const started_by = event.started_by;
-
-                  const networkConfiguration =
-                      dsdata.services[0].networkConfiguration;
-                  const launchType = dsdata.services[0].launchType;
-
-                  const params = {
-                    taskDefinition : taskDefinition,
-                    networkConfiguration : networkConfiguration,
-                    cluster : ecs_cluster,
-                    count : 1,
-                    launchType : launchType,
-                    startedBy : started_by,
-                    overrides : event.overrides
-                  };
-
-                  ecs.runTask(params, (err, data) => {
-                    if (err) {
-                      if (err.code == 'ConditionalCheckFailedException') {
-                        callback('duplicated execution: ' +
-                                 JSON.stringify(event));
-                      } else {
-                        callback(err);
-                      }
-                    } else {
-                      console.log("Successfully started taskDefinition " +
-                                  taskDefinition + "\n" + JSON.stringify(data));
-                      callback.null("Successfully started taskDefinition " +
-                                    taskDefinition + "\n" +
-                                    JSON.stringify(data));
-                    }
-                  })
-                }
-              })
+            callback(err);
         }
-      });
+    } else {
+        console.log("Successfully started taskDefinition " + taskDefinition + "\n" + JSON.stringify(data));
+        callback.null("Successfully started taskDefinition " + taskDefinition + "\n" + JSON.stringify(data));
+    }
 };
