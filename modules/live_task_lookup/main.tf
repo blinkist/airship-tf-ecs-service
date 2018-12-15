9fe2defaa5e1c0c0eb02bbfb404b4bc7413a737f
Anton Babenko <anton@antonbabenko.com>
2018-12-15 22:17:33 +0100

    Disable lambda function entirely when using datasource

--- modules/live_task_lookup/main.tf
+++ modules/live_task_lookup/main.tf
@@ -2,7 +2,7 @@
 # lookup_type lambda
 #
 resource "aws_lambda_function" "lambda_lookup" {
-  count            = "${var.create ? 1 : 0}"
+  count            = "${var.create && var.lookup_type == "lambda" ? 1 : 0}"
   function_name    = "${basename(var.ecs_cluster_id)}-${var.ecs_service_name}-lambda-lookup"
   handler          = "index.handler"
   runtime          = "nodejs8.10"
