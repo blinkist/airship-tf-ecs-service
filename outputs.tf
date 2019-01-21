5251c7e66ec839ec04f36fe0f4fc0ae62cc90326
Germaniero <lukasz.german@gmail.com>
2019-01-21 10:56:17 +0100

    Post review adjustments

--- outputs.tf
+++ outputs.tf
@@ -10,12 +10,12 @@ output "lb_target_group_arn" {
   value = "${module.alb_handling.lb_target_group_arn}"
 }
 
-output "task execution role arn" {
+output "task_execution_role_arn" {
   value = "${module.iam.ecs_task_execution_role_arn}"
 }
 
 output "aws_ecs_task_definition_arn" {
-  value = "${module.ecs_task_definition.aws_ecs_task_definition_arn}"
+  value = "${module.ecs_task_definition_selector.selected_task_definition_for_deployment}"
 }
 
 output "aws_ecs_task_definition_family" {
