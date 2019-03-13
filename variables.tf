6e7d3af481c7ea2bf6c68a80f7be11aef3177021
Jamie Nelson <jamie@theinstillery.com>
2019-03-14 11:09:00 +1300

    fmt

--- variables.tf
+++ variables.tf
@@ -346,15 +346,15 @@ variable "host_path_volumes" {
   type    = "list"
   default = []
 
-## Example:
-    # host_path_volumes = [{
-    #   name = "foo",
-    #   host_path = "/tmp/foo"
-    # },
-    # {
-    #   name = "bar",
-    #   host_path = "/tmp/bar"
-    # },]
+  ## Example:
+  # host_path_volumes = [{
+  #   name = "foo",
+  #   host_path = "/tmp/foo"
+  # },
+  # {
+  #   name = "bar",
+  #   host_path = "/tmp/bar"
+  # },]
 }
 
 # list of mount points to add to every container in the task
@@ -362,39 +362,38 @@ variable "mountpoints" {
   type    = "list"
   default = []
 
-## Example 
-    # mountpoints = [{
-    #   sourceVolume = "foo",
-    #   containerPath = "/foo",
-    #   readOnly = "false"
-    # },
-    # {
-    #   sourceVolume = "bar",
-    #   containerPath = "/bar",
-    #   readOnly = "false"
-    # },]
+  ## Example 
+  # mountpoints = [{
+  #   sourceVolume = "foo",
+  #   containerPath = "/foo",
+  #   readOnly = "false"
+  # },
+  # {
+  #   sourceVolume = "bar",
+  #   containerPath = "/bar",
+  #   readOnly = "false"
+  # },]
 }
 
 variable "ecs_cron_tasks" {
   type    = "list"
   default = []
 
-## ecs_cron_tasks holds a list of maps defining the scheduled jobs which need to run
-## Example
+  ## ecs_cron_tasks holds a list of maps defining the scheduled jobs which need to run
+  ## Example
 
-#  ecs_cron_tasks = [{
-#     # name of the scheduled task
-#     job_name  = "vacuum_db"
-#     
-#     # expression defined in 
-#     # http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
-#     schedule_expression  = "cron(0 12 * * ? *)"
-#
-#     # command defines the command which needs to run inside the docker container
-#     command = "python vacuum_db.py"
-#
-#   },]
-
+  #  ecs_cron_tasks = [{
+  #     # name of the scheduled task
+  #     job_name  = "vacuum_db"
+  #     
+  #     # expression defined in 
+  #     # http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
+  #     schedule_expression  = "cron(0 12 * * ? *)"
+  #
+  #     # command defines the command which needs to run inside the docker container
+  #     command = "python vacuum_db.py"
+  #
+  #   },]
 }
 
 variable "service_discovery_enabled" {
